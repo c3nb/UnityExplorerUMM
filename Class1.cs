419e170f83239f35c2bea3f287b19d81285ba014
C#Newbie <73321185+c3nb@users.noreply.github.com>
2021-09-20 19:16:15 +0900

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityModManagerNet;
+
+namespace UnityExplorer
+{
+    public class Main
+    {
+        public static bool Load(UnityModManager.ModEntry modEntry) { ExplorerStandalone.CreateInstance(); return true; }
+    }
+}
